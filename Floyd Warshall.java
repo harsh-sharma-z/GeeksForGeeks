57ca307368b8d2568fe8cd52b2d6bff114d3ab7a
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2023-04-02 09:50:35 +0530

    Update Floyd Warshall.java

--- Floyd Warshall.java
+++ Floyd Warshall.java
@@ -38,19 +38,48 @@ class GFG
 
 class Solution
 {
-    public void shortest_distance(int[][] mat)
+    public void shortest_distance(int[][] matrix)
     {
         // Code here 
-        int n=mat.length;
-        for(int k=0;k<n;k++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(mat[i][k]==-1 || mat[k][j]==-1)continue;
-                    if(mat[i][j]==-1 || mat[i][k]+mat[k][j]<mat[i][j]){
-                        mat[i][j]=mat[i][k]+mat[k][j];
-                    }
+        int n=matrix.length;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==-1)
+                {
+                    matrix[i][j]=(int)(1e9);
+                }
+                
+                if(i==j)
+                matrix[i][j]=0;
+            }
+        }
+        
+        
+        for(int k=0;k<n;k++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    matrix[i][j]=Math.min(matrix[i][j],(matrix[i][k]+matrix[k][j]));
                 }
             }
         }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==(1e9))
+                {
+                    matrix[i][j]=-1;
+                }
+              
+            }
+        }
+        
     }
 }
