48ef10713d243cc580071696a71e8397dfc89b7b
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-04-13 22:25:45 +0530

    Create Reverse Bits.java

---
+++ Reverse Bits.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            Long X = Long.parseLong(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.reversedBits(X));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static Long reversedBits(Long x) {
+        // code here
+        Long res=0l;
+        for(int i=0;i<32;i++){
+         res= (res<<1)+ (x%2);
+         x/=2;
+        }
+        return res;
+    }
+};
