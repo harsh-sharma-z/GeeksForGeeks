c89109cd850161dfef03bbae0d23877a2da4d4ca
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-04-22 22:53:03 +0530

    Create Row with minimum number of 1's.java

---
+++ Row with minimum number of 1's.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s[] = read.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            int A[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String S[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    A[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.minRow(N, M, A));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minRow(int n, int m, int a[][]) {
+        // code here
+        int ans = 0, min = Integer.MAX_VALUE;
+        for(int i = 0 ; i< n ; i++){
+            int sum = 0;
+            for(int j = 0 ; j < m ; j++){
+                sum += a[i][j];
+            }
+            if(sum < min){
+                min = sum;
+                ans = i;
+            }
+        }
+        return ans + 1;
+    }
+};
