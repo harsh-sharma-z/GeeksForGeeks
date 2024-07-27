7b4906fd51ac49cdfe2f6ed7143b3196a850dd81
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-07-27 23:04:11 +0530

    Create Form a palindrome.java

---
+++ Form a palindrome.java
@@ -0,0 +1,32 @@
+class Solution{
+    static int dp[][];
+    static int countMin(String s)
+    {
+        int n=s.length();
+     dp=new int[n+1][n+1];
+      for(int row[]:dp){
+        Arrays.fill(row,-1);
+      }
+      if(n==1){
+        return 0;
+      }
+      int i=0;
+      int j=n-1;
+      return n-lps(s,i,j);
+    }
+   static int lps(String s,int i,int j){
+        if(i>j){
+            return 0;
+        }
+        if(i==j) return 1;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j]=2+lps(s,i+1,j-1);
+        }else{
+            return dp[i][j]=Math.max(lps(s,i+1,j),lps(s,i,j-1));
+        }
+    }
+}
+
