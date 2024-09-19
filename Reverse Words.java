c1658aee2fc52ba5bfbe7f87d347555536d5881c
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-09-19 22:23:30 +0530

    Create Reverse Words.java

---
+++ Reverse Words.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    // Function to reverse words in a given string.
+    String reverseWords(String str) {
+        // code here
+        String text[]=str.split("\\.");
+        List<String>al=new ArrayList<>(Arrays.asList(text));
+        Collections.reverse(al);
+        return String.join(".",al);
+    }
+}
