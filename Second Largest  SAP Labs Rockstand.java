53b81e74fc34ce5784d1e937de3f48e7d6897877
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-11-15 21:48:11 +0530

    Create Second Largest  SAP Labs Rockstand.java

---
+++ Second Largest  SAP Labs Rockstand.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] arr1Str = sc.nextLine().split(" ");
+            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
+            Solution ob = new Solution();
+            int ans = ob.getSecondLargest(arr);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+       int n=arr.length,sl=0;
+        Arrays.sort(arr);
+        for(int i=2;i<=n;i++)
+        {
+            if(arr[n-1]!=arr[n-i])
+            {
+                sl=arr[n-i];
+                return sl;
+            }
+        }
+        return -1;
+    }
+}
