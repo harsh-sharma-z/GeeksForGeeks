39ef1afad6244201244328318234dba7b9f00ed9
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-11-18 22:14:32 +0530

    Create Rotate Array.java

---
+++ Rotate Array.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        
+         Queue<Integer> qu = new LinkedList<>();
+        
+        int m = d%arr.length;
+        int i=0;
+        while(i<m){
+            qu.add(arr[i]);
+            i++;
+        }
+        
+        int j=0;
+        while(i<arr.length){
+            arr[j]=arr[i];
+            i++;
+            j++;
+        }
+        
+        while(!qu.isEmpty()){
+            arr[j] = qu.poll();
+            j++;
+        }
+    }
+}
