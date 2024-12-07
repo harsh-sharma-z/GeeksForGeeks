21b0a0529efc6a8fbbf3e331ab6155e7432fb4f1
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2024-12-07 23:24:27 +0530

    Create Count Inversions.java

---
+++ Count Inversions.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().inversionCount(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int inversionCount(int arr[]) {
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+
+    static int mergeSort(int arr[], int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count += mergeSort(arr, left, mid);
+            count += mergeSort(arr, mid + 1, right);
+            count += merge(arr, left, mid, right);
+        }
+        return count;
+    }
+
+    static int merge(int arr[], int left, int mid, int right) {
+        int count = 0;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int leftArr[] = new int[n1];
+        int rightArr[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+                count += n1 - i;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+
+        return count;
+    }
+}
